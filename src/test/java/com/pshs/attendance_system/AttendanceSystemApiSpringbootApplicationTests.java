584db9f498ca0fb5cd0a5b694e3e597f52a39dc4
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-04-11 14:23:53 +0800

    Initial commit

---
+++ src/test/java/com/pshs/attendance_system/AttendanceSystemApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pshs.attendance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceSystemApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
