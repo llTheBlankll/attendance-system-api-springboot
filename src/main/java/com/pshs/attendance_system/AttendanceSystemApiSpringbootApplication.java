c011349f8c22888fceaffabd3f30e945eba93e61
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-04-11 14:23:53 +0800

    Initial commit

---
+++ src/main/java/com/pshs/attendance_system/AttendanceSystemApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pshs.attendance_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceSystemApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceSystemApiSpringbootApplication.class, args);
+	}
+
+}
