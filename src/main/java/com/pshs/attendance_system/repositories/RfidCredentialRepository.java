cde377964693549b4ef67a9a135c679bfc8570f8
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-04-20 17:41:18 +0800

    Rename RFID Credential Repository

--- src/main/java/com/pshs/attendance_system/repositories/RfidCredentialRepository.java
+++ src/main/java/com/pshs/attendance_system/repositories/RfidCredentialRepository.java
@@ -1,9 +1,9 @@
 package com.pshs.attendance_system.repositories;
 
-import com.pshs.attendance_system.entities.RfidCredential;
+import com.pshs.attendance_system.entities.RFIDCredential;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RfidCredentialRepository extends JpaRepository<RfidCredential, Integer> {
+public interface RfidCredentialRepository extends JpaRepository<RFIDCredential, Integer> {
 }
