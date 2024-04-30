df45d3d5c194ca1c1a5f227acf2eecafe1a3a5f8
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-04-30 21:55:22 +0800

    Mock data

---
+++ src/main/resources/db/mock_data/Grade Levels.sql
@@ -0,0 +1,12 @@
+insert into grade_levels (name, strand) values ('Grade 1', 7);
+insert into grade_levels (name, strand) values ('Grade 2', 10);
+insert into grade_levels (name, strand) values ('Grade 3', 5);
+insert into grade_levels (name, strand) values ('Grade 4', 5);
+insert into grade_levels (name, strand) values ('Grade 5', 5);
+insert into grade_levels (name, strand) values ('Grade 6', 6);
+insert into grade_levels (name, strand) values ('Grade 7', 11);
+insert into grade_levels (name, strand) values ('Grade 8', 9);
+insert into grade_levels (name, strand) values ('Grade 9', 11);
+insert into grade_levels (name, strand) values ('Grade 10', 2);
+insert into grade_levels (name, strand) values ('Grade 11', 4);
+insert into grade_levels (name, strand) values ('Grade 12', 3);
---
+++ src/main/resources/db/mock_data/Strand.sql
@@ -0,0 +1,11 @@
+insert into strand (name) values ('STEM');
+insert into strand (name) values ('ABM');
+insert into strand (name) values ('HUMSS');
+insert into strand (name) values ('GAS');
+insert into strand (name) values ('TVL');
+insert into strand (name) values ('ARTS and DESIGN');
+insert into strand (name) values ('Agri-Fishery Arts');
+insert into strand (name) values ('Home Economics');
+insert into strand (name) values ('Industrial Arts');
+insert into strand (name) values ('Information and Communications Technology (ICT)');
+insert into strand (name) values ('TVL Maritime');
