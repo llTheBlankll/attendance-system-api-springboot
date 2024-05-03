420eadbb93c557a8ee0165900644b5380076e6ce
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-05-03 12:36:19 +0800

    Enable caching in the application

--- src/main/java/com/pshs/attendance_system/AttendanceSystemApplication.java
+++ src/main/java/com/pshs/attendance_system/AttendanceSystemApplication.java
@@ -25,8 +25,10 @@ package com.pshs.attendance_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class AttendanceSystemApplication {
 
 	public static void main(String[] args) {
