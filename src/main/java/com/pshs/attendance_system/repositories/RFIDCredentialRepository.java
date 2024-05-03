b275892d9521cb40046fe147fd09f1cc28412f0f
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-05-03 09:54:13 +0800

    Implement the service of RFID Credential

--- src/main/java/com/pshs/attendance_system/repositories/RFIDCredentialRepository.java
+++ src/main/java/com/pshs/attendance_system/repositories/RFIDCredentialRepository.java
@@ -24,9 +24,19 @@
 package com.pshs.attendance_system.repositories;
 
 import com.pshs.attendance_system.entities.RFIDCredential;
+import com.pshs.attendance_system.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface RFIDCredentialRepository extends JpaRepository<RFIDCredential, Integer> {
+	@Query("select r from RFIDCredential r where r.lrn = ?1")
+	Optional<RFIDCredential> getRFIDCredentialByStudent(@NonNull Student lrn);
+
+	@Query("select r from RFIDCredential r where r.lrn.id = ?1")
+	Optional<RFIDCredential> getRFIDCredentialByStudentId(@NonNull Long id);
 }
