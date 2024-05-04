1146051e1ef86ad3e57b15bee9b76d189618fcfe
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-05-04 18:25:22 +0800

    Gradelevel

--- src/main/java/com/pshs/attendance_system/controllers/GradeLevelController.java
+++ src/main/java/com/pshs/attendance_system/controllers/GradeLevelController.java
@@ -25,17 +25,20 @@ package com.pshs.attendance_system.controllers;
 
 import com.pshs.attendance_system.dto.GradeLevelDTO;
 import com.pshs.attendance_system.entities.GradeLevel;
+import com.pshs.attendance_system.enums.ExecutionStatus;
 import com.pshs.attendance_system.services.GradeLevelService;
+import io.swagger.v3.oas.annotations.extensions.Extension;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.http.RequestEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @Tag(
@@ -46,25 +49,67 @@ import org.springframework.web.bind.annotation.RestController;
 public class GradeLevelController {
 
 	private static final Logger logger = LogManager.getLogger(GradeLevelController.class);
-	private final GradeLevelService gradeLevelService
+	private final GradeLevelService gradeLevelService;
 
 	public GradeLevelController(GradeLevelService gradeLevelService) {
 		this.gradeLevelService = gradeLevelService;
 	}
 
 	@GetMapping("/")
-	public RequestEntity<Page<GradeLevelDTO>> getAllGradeLevels(
+	public ResponseEntity<Page<GradeLevelDTO>> getAllGradeLevels(
 		@RequestParam(defaultValue = "0") Integer page,
 		@RequestParam(defaultValue = "10") Integer size
 	) {
 		logger.info("Fetching all grade levels");
-		return RequestEntity.ok(gradeLevelService.getAllGradeLevels(page, size));
+		return ResponseEntity.ok(
+			gradeLevelService.getAllGradeLevels(page, size).map(
+				GradeLevel::toDTO
+			)
+		);
 	}
 
-	@GetMapping(/{id})
-	public RequestEntity<GradeLevelDTO> getGradeLevelById(@PathVariable Long id) {
+	@PostMapping("/")
+	public ResponseEntity<Map<String, ExecutionStatus>> createGradeLevel(@RequestBody GradeLevelDTO gradeLevelDTO) {
+		logger.info("Creating grade level: {}", gradeLevelDTO);
+		return ResponseEntity.ok(
+			Map.of(
+				"status",
+				gradeLevelService.createGradeLevel(gradeLevelDTO.toEntity())
+			)
+		);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Map<String, ExecutionStatus>> deleteGradeLevel(@PathVariable Integer id) {
+		logger.info("Deleting grade level by id: {}", id);
+		return ResponseEntity.ok(
+			Map.of(
+				"status",
+				gradeLevelService.deleteGradeLevel(id)
+			)
+		);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<GradeLevelDTO> getGradeLevelById(@PathVariable Integer id) {
 		logger.info("Fetching grade level by id: {}", id);
-		return RequestEntity.ok(gradeLevelService.getGradeLevelById(id));
+		return ResponseEntity.ok(
+			gradeLevelService.getGradeLevel(id).toDTO()
+		);
 	}
 
+	@ApiResponse(
+		responseCode = "200",
+		description = "Grade Level updated successfully"
+	)
+	@PutMapping("/{id}")
+	public ResponseEntity<Map<String, ExecutionStatus>> updateGradeLevel(@PathVariable Integer id, @RequestBody GradeLevelDTO gradeLevelDTO) {
+		logger.info("Updating grade level by id: {}", id);
+		return ResponseEntity.ok(
+			Map.of(
+				"status",
+				gradeLevelService.updateGradeLevel(id, gradeLevelDTO.toEntity())
+			)
+		);
+	}
 }
