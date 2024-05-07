d6599435ac90b3b01f13ca46af925c92bf8af017
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-05-07 19:05:33 +0800

    RabbitMQ, we can now receive messages and send logs and notifications

---
+++ src/main/java/com/pshs/attendance_system/broker/AttendanceReceiver.java
@@ -0,0 +1,73 @@
+/*
+ * COPYRIGHT (C) 2024 VINCE ANGELO BATECAN
+ *
+ * PERMISSION IS HEREBY GRANTED, FREE OF CHARGE, TO STUDENTS, FACULTY, AND STAFF OF PUNTURIN SENIOR HIGH SCHOOL TO USE THIS SOFTWARE FOR EDUCATIONAL PURPOSES ONLY.
+ *
+ * THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE.
+ *
+ * MODIFICATIONS:
+ *
+ * ANY MODIFICATIONS OR DERIVATIVE WORKS OF THE SOFTWARE SHALL BE CONSIDERED PART OF THE SOFTWARE AND SHALL BE SUBJECT TO THE TERMS AND CONDITIONS OF THIS LICENSE.
+ * ANY PERSON OR ENTITY MAKING MODIFICATIONS TO THE SOFTWARE SHALL ASSIGN AND TRANSFER ALL RIGHT, TITLE, AND INTEREST IN AND TO SUCH MODIFICATIONS TO VINCE ANGELO BATECAN.
+ * VINCE ANGELO BATECAN SHALL OWN ALL INTELLECTUAL PROPERTY RIGHTS IN AND TO SUCH MODIFICATIONS.
+ *
+ * NO COMMERCIAL USE:
+ *
+ * THE SOFTWARE SHALL NOT BE SOLD, RENTED, LEASED, OR OTHERWISE COMMERCIALLY EXPLOITED. THE SOFTWARE IS INTENDED FOR PERSONAL, NON-COMMERCIAL USE ONLY WITHIN PUNTURIN SENIOR HIGH SCHOOL.
+ *
+ * NO WARRANTIES:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.pshs.attendance_system.broker;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pshs.attendance_system.dto.RFIDCredentialDTO;
+import com.pshs.attendance_system.dto.StudentRFID;
+import com.pshs.attendance_system.entities.RFIDCredential;
+import com.pshs.attendance_system.services.AttendanceService;
+import com.pshs.attendance_system.services.RFIDCredentialService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AttendanceReceiver {
+
+	private final RabbitTemplate rabbitTemplate;
+	private final AttendanceService attendanceService;
+	private final RFIDCredentialService rfidCredentialService;
+	private static final Logger logger = LogManager.getLogger(AttendanceReceiver.class);
+
+	public AttendanceReceiver(RabbitTemplate rabbitTemplate, AttendanceService attendanceService, RFIDCredentialService rfidCredentialService) {
+		this.rabbitTemplate = rabbitTemplate;
+		this.attendanceService = attendanceService;
+		this.rfidCredentialService = rfidCredentialService;
+	}
+
+
+
+	@RabbitListener(queues = "#{attendanceQueue.name}")
+	public void receive(String in) {
+		try {
+			ObjectMapper mapper = new ObjectMapper();
+			StudentRFID studentRFID = mapper.readValue(in, StudentRFID.class);
+			RFIDCredential rfidCredential = rfidCredentialService.getRFIDCredentialByStudentId(studentRFID.getLrn());
+
+			if (rfidCredential.getLrn() != null) {
+				attendanceService.createAttendance(rfidCredential.getLrn().getId());
+			}
+
+			// ! If success, send a message to the topic exchange in topic attendance-notifications
+			rabbitTemplate.convertAndSend("amq.topic", "attendance-notifications", "SUCCESS");
+			logger.info("Received message: {}", studentRFID.getLrn());
+		} catch (Exception e) {
+			rabbitTemplate.convertAndSend("amq.topic", "attendance-logs", "ERROR: " + e.getMessage());
+			logger.info("Error receiving message: {}", e.getMessage());
+		}
+	}
+}
--- src/main/java/com/pshs/attendance_system/broker/RabbitMQImplementation.java
+++ src/main/java/com/pshs/attendance_system/broker/RabbitMQImplementation.java
@@ -22,23 +22,71 @@
  */
 
 package com.pshs.attendance_system.broker;
+
+import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableRabbit
 public class RabbitMQImplementation {
+	static final String ATTENDANCE_EXCHANGE = "attendance-exchange";
+	static final String ATTENDANCE_QUEUE = "attendance";
 
-	private final String TOPIC = "attendance";
-	private final String QUEUE_NAME = "attendance-queue";
+	// Region: Attendance Queue
 
 	@Bean
-	Queue queue() {
-		return new Queue(QUEUE_NAME, false);
+	Queue attendanceQueue() {
+		return new Queue(ATTENDANCE_QUEUE, true);
 	}
 
 	@Bean
-	Queue topic() {
-		return new Queue(TOPIC, false);
+	TopicExchange attendanceTopicExchange() {
+		return new TopicExchange(ATTENDANCE_EXCHANGE);
 	}
 
-	
+	@Bean
+	public Binding attendanceBinding() {
+		return new Binding(ATTENDANCE_QUEUE, Binding.DestinationType.QUEUE, "amq.topic", "attendance", null);
+	}
+
+	// End Region: Attendance Queue
+
+	// Region: Attendance Notification Queue
+
+	@Bean
+	Queue attendanceNotificationQueue() {
+		return new Queue("attendance-notifications", true);
+	}
+
+	@Bean
+	TopicExchange attendanceNotificationTopicExchange() {
+		return new TopicExchange("attendance-notifications-exchange");
+	}
+
+	@Bean
+	public Binding attendanceNotificationsBinding() {
+		return new Binding("attendance-notifications", Binding.DestinationType.QUEUE, "amq.topic", "attendance-notifications", null);
+	}
+
+	// End Region: Attendance Notification Queue
+
+	// Region: Attendance Logs
+	@Bean
+	Queue attendanceLogsQueue() {
+		return new Queue("attendance-logs", true);
+	}
+
+	@Bean
+	TopicExchange attendanceLogsTopicExchange() {
+		return new TopicExchange("attendance-logs-exchange");
+	}
+
+	@Bean
+	public Binding attendanceLogsBinding() {
+		return new Binding("attendance-logs", Binding.DestinationType.QUEUE, "amq.topic", "attendance-logs", null);
+	}
 }
