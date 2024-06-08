9c6924b9ae185627fcfca7f626be57c12fc31445
Vince Angelo Batecan <llTheBlankll@gmail.com>
2024-06-08 13:28:21 +0800

    Update AttendanceWebSocketHandler and WebSocketHandler for Real-time Notifications
    This commit updates the AttendanceWebSocketHandler and WebSocketHandler classes to support real-time notifications.
    
    In AttendanceWebSocketHandler, a new method notifySubscribers has been added to notify subscribers of attendance results. The method checks if the message from the attendance result is different from the specified comparison message before sending the notification.
    
    The handleMessage method has been updated to handle both attendance in and attendance out scenarios. After retrieving the RFIDCredential from the database, the method checks if it exists. If not, an error message is sent to the client. For attendance in and attendance out scenarios, the corresponding attendance logic is executed, and the result is sent to the client. The notifySubscribers method is called to notify subscribers of the attendance result.
    
    In WebSocketHandler, the registerWebSocketHandlers method has been updated to include a new WebSocketHandler for real-time notifications. The RealTimeNotificationsHandler is added to handle "/wsocket/notification-subscribers" connections.
    
    These changes enable real-time notifications for attendance results, allowing clients to receive updates without the need for manual refreshes.
    
    fixes #9

--- src/main/java/com/pshs/attendance_system/websocket/WebSocketHandler.java
+++ src/main/java/com/pshs/attendance_system/websocket/WebSocketHandler.java
@@ -5,6 +5,8 @@ package com.pshs.attendance_system.websocket;
 import com.pshs.attendance_system.services.AttendanceService;
 import com.pshs.attendance_system.services.RFIDCredentialService;
 import com.pshs.attendance_system.websocket.handlers.AttendanceWebSocketHandler;
+import com.pshs.attendance_system.websocket.handlers.RealTimeNotificationsHandler;
+import com.pshs.attendance_system.websocket.services.RealTimeNotificationSubscribers;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -16,14 +18,17 @@ public class WebSocketHandler implements WebSocketConfigurer {
 
 	private final AttendanceService attendanceService;
 	private final RFIDCredentialService rfidCredentialService;
+	private final RealTimeNotificationSubscribers realTimeNotificationSubscribers;
 
-	public WebSocketHandler(AttendanceService attendanceService, RFIDCredentialService rfidCredentialService) {
+	public WebSocketHandler(AttendanceService attendanceService, RFIDCredentialService rfidCredentialService, RealTimeNotificationSubscribers realTimeNotificationSubscribers) {
 		this.attendanceService = attendanceService;
 		this.rfidCredentialService = rfidCredentialService;
+		this.realTimeNotificationSubscribers = realTimeNotificationSubscribers;
 	}
 
 	@Override
 	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-		registry.addHandler(new AttendanceWebSocketHandler(attendanceService, rfidCredentialService), "/wsocket/attendance").setAllowedOrigins("*");
+		registry.addHandler(new AttendanceWebSocketHandler(attendanceService, rfidCredentialService, realTimeNotificationSubscribers), "/wsocket/attendance").setAllowedOrigins("*");
+		registry.addHandler(new RealTimeNotificationsHandler(realTimeNotificationSubscribers), "/wsocket/notification-subscribers");
 	}
 }
